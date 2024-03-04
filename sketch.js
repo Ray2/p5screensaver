d363aab121f5bc1f41d941a0511cc891009715c2
Ray2 <RayCogliano1@gmail.com>
2024-03-04 01:28:53 -0500

    Initial commit

---
+++ sketch.js
@@ -0,0 +1,34 @@
+
+let t = 0;
+let numLines = 100;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  strokeWeight(2);
+  stroke(255);
+  frameRate(15);
+}
+
+function draw() {
+  background(0);
+  translate(width / 2, height / 2);
+
+  let amplitude = width / 10;
+
+  for (let i = 0; i < numLines; i++) {
+    let x1 = sin((t + i) / 10) * amplitude;
+    let y1 = cos((-t + i) / 10) * amplitude + sin(((t + 1) / 10) * 50);
+
+    let x2 = sin((t + i) / 20) * (amplitude * 2) + cos(t + 1) * 10;
+    let y2 = cos((-t + i) / 10) * (amplitude * 2);
+
+    line(x1, y1, x2, y2);
+  }
+
+  t += 0.1;
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  background(0);
+}
